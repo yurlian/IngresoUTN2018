ffcdbf23e2d533c29b288601e376da4ff84017fb
yurlian <31257437+yurlian@users.noreply.github.com>
2018-02-09 22:18:51 -0300

    ejercicios hasta el 7 resuelto

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,11 @@
 function Mostrar()
 {
 //tomo la edad  
+    var edad = document.getElementById("edad").value; 
 
+    if(edad >12 && edad <17){
+        alert("Usted es un adolescente");
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad = document.getElementById("edad").value;
 
+if(edad > 17){
+    alert("Usted es mayor de edad");
+}else if(edad < 12){
+    alert("Usted es un niño");
+}else{
+    alert("Usted es adolescente");
+}
 
 
 
